6498d38b896aaaf200d11e328f09b4328f87290d
jtantongco <jtantongco@gmail.com>
2013-07-10 02:21:28 -0700

    Some week 3 files

---
+++ Week3/alias_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#alias setup
+
+date
+echo "Starting alias configuration"
+
+alias ll='ls -alrth'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias ln='ln -si'
+alias sed='sed -i'
+
+alias
+echo "Your aliases are set-up, Jeremiah"
